ca8daa2c3796900464dc5e1973a5e54ebc3bfbde
maetaro <maetaro@users.noreply.github.com>
2021-09-18 08:26:58 +0900

    Add library project.

---
+++ WindowsShortcutParser/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WindowsShortcutParser
+{
+    public class Class1
+    {
+        public static string[] Parse()
+        {
+            return new[] { "", "" };
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
