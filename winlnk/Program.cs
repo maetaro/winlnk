fcf6587bd6f114c1f0006b2f93a8f7cbfab5dd6a
maetaro <maetaro@users.noreply.github.com>
2021-09-18 08:27:33 +0900

    add console app project.

---
+++ winlnk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace winlnk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
