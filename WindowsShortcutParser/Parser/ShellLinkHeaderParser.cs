fd98ecd7f50b8f3b86d0c57a61032e515d2cd32a
maetaro <maetaro0614@gmail.com>
2021-09-24 09:39:49 +0900

    add serialize method.

--- WindowsShortcutParser/Parser/ShellLinkHeaderParser.cs
+++ WindowsShortcutParser/Parser/ShellLinkHeaderParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 using WindowsShortcutParser.Entity;
 using WindowsShortcutParser.Utility;
 
@@ -6,7 +7,7 @@ namespace WindowsShortcutParser.Parser
 {
     public static class ShellLinkHeaderParser
     {
-        public static ShellLinkHeader Parse(BinaryReaderEx reader)
+        public static ShellLinkHeader Deserialize(BinaryReaderEx reader)
         {
             var entity = new ShellLinkHeader();
             entity.HeaderSize = reader.ReadInt32();
@@ -24,6 +25,24 @@ namespace WindowsShortcutParser.Parser
             entity.Reserved2 = reader.ReadBytes(4);
             entity.Reserved3 = reader.ReadBytes(4);
             return entity;
+        }
+
+        public static void Serialize(System.IO.Stream stream, ShellLinkHeader entity)
+        {
+            stream.Write(BitConverter.GetBytes(entity.HeaderSize), 0, 4);
+            stream.Write(entity.LinkCLSID.ToByteArray(), 0, 16);
+            stream.Write(entity.LinkFlags.ToByteArray(), 0, 4);
+            stream.Write(entity.FileAttributes.ToByteArray(), 0, 4);
+            stream.Write(BitConverter.GetBytes(entity.CreationTime.ToFileTime()), 0, 8);
+            stream.Write(BitConverter.GetBytes(entity.AccessTime.ToFileTime()), 0, 8);
+            stream.Write(BitConverter.GetBytes(entity.WriteTime.ToFileTime()), 0, 8);
+            stream.Write(BitConverter.GetBytes(entity.FileSize), 0, 4);
+            stream.Write(BitConverter.GetBytes(entity.IconIndex), 0, 4);
+            stream.Write(BitConverter.GetBytes(entity.ShowCommand), 0, 4);
+            stream.Write(entity.HotKey.ToByteArray(), 0, 2);
+            stream.Write(entity.Reserved1, 0, 2);
+            stream.Write(entity.Reserved2, 0, 4);
+            stream.Write(entity.Reserved3, 0, 4);
         }
     }
 }
