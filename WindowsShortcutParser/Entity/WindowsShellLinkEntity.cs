8998efe159d93497dd3863e53dbdf883f30aef7b
maetaro <maetaro0614@gmail.com>
2021-09-24 09:38:55 +0900

    change default value of some objects. to null.

--- WindowsShortcutParser/Entity/WindowsShellLinkEntity.cs
+++ WindowsShortcutParser/Entity/WindowsShellLinkEntity.cs
@@ -1,10 +1,16 @@
-﻿namespace WindowsShortcutParser.Entity
+﻿using System;
+
+namespace WindowsShortcutParser.Entity
 {
     public class WindowsShellLinkEntity : EntityBase
     {
-        public ShellLinkHeader ShellLinkHeader { get; internal set; } = new();
-        public LinkTargetIDList LinkTargetIDList { get; internal set; } = new();
-        public LinkInfo LinkInfo { get; internal set; } = new();
-        public StringData StringData { get; set; } = new();
+        public ShellLinkHeader ShellLinkHeader { get; internal set; }
+        public LinkTargetIDList LinkTargetIDList { get; internal set; }
+        public LinkInfo LinkInfo { get; internal set; }
+        public StringData StringData { get; set; }
+        public byte[] ToByteArray()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
