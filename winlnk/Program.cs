c19d289d5caf58e356a4ee31a001459697a3f988
maetaro <maetaro0614@gmail.com>
2021-10-24 10:43:41 +0900

    Update Program.cs

--- winlnk/Program.cs
+++ winlnk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace winlnk
 {
@@ -6,7 +7,35 @@ namespace winlnk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                if (args.Length > 2)
+                {
+                    return;
+                }
+                if (args.Length == 1)
+                {
+                    string path = args[0];
+                    Show(path);
+                    return;
+                }
+                else
+                {
+                    string pathJson = args[0];
+                    string pathLnk = args[1];
+                    var entity = WindowsShortcutParser.Parser.WindowsShortcutParser.FromJsonFile(pathJson);
+                    File.WriteAllBytes(pathLnk, entity.ToByteArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private static void Show(string path)
+        {
+            //var entity = WindowsShortcutParser.Parser.WindowsShortcutParser.Parse(path);
+            //Console.WriteLine(entity.ToJsonString());
         }
     }
 }
